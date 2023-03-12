6d1ca84622ceaa5563c2db71dc8d4621f85b3a38
Alyosha Romanov <tato.romanov@gmail.com>
2023-03-11 22:30:12 -0700

    mp-2: added psychedelic shader and cleaned up code

--- mp-2/shaders/general-vertex.glsl
+++ mp-2/shaders/general-vertex.glsl
@@ -1,9 +1,8 @@
 attribute vec3 vertexPosition;
 attribute vec4 vertexColor;
 uniform mat4 mvMatrix;
-uniform mat4 pMatrix;
 varying vec4 color;
 void main() {
-    gl_Position = mvMatrix *pMatrix * vec4(vertexPosition, 1.0);
+    gl_Position = mvMatrix * vec4(vertexPosition, 1.0);
     color = vertexColor;
 }
